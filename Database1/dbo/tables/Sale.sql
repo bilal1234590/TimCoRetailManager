b1b4ee9740f40c505021597b00a11755910e7ee0
bilal1234590 <75792984+bilal1234590@users.noreply.github.com>
2022-08-20 17:09:07 +0530

    Register DB Tables
    The tables for the basic register
    system.

---
+++ Database1/dbo/tables/Sale.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Sale]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [CashierId] NVARCHAR(128) NULL, 
+    [SaleDate] DATETIME2 NULL, 
+    [SabTotal] MONEY NULL, 
+    [Tax] MONEY NULL, 
+    [Total] MONEY NULL
+)
